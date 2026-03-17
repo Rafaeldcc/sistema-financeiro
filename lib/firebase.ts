9b2ce0f24764784a7982f5884369f42a92104707
Rafael da Costa Carvalho <rafaeldacostacarvalho.tj@gmail.com>
2026-03-17 16:36:59 -0300

    fix firebase export app

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,4 +1,8 @@
-import { initializeApp } from "firebase/app";
+// ===============================
+// FIREBASE CONFIG
+// ===============================
+
+import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
@@ -11,7 +15,13 @@ const firebaseConfig = {
   appId: "1:315301860317:web:6045ea403414f598d01c34",
 };
 
-const app = initializeApp(firebaseConfig);
+// 🔥 Evita erro de múltiplas inicializações no Vercel
+const app = getApps().length === 0
+  ? initializeApp(firebaseConfig)
+  : getApps()[0];
+
+// ✅ EXPORTANDO TUDO CERTO
+export { app };
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
